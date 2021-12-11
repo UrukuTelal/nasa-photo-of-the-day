f4206370a3578a6fd4a586f85045759ee57fa790
UrukuTelal <ao.bier1101@gmail.com>
2021-12-10 22:29:52 -0800

    I have a picture from the api displaying on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+
+
 
 function App() {
+
+  const [pics, setPics] = useState([]);
+
+useEffect(() => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=0Cy7DjwTZ5Df5h9WiTizcFFsJx3B1vIHxwezah4M')
+  .then(resp => {
+    console.log(resp.data);
+    setPics(resp.data.url);
+    
+  })
+  .catch(err => console.error(err))
+
+}, [])
+
   return (
     <div className="App">
+      <h1>Nasa Pic</h1>
+      <img src={pics}></img>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!

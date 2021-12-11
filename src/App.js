0f39d6f66b68df3a0541f65ef81b22db468efbf4
UrukuTelal <ao.bier1101@gmail.com>
2021-12-11 00:46:46 -0800

    added a title and text

--- src/App.js
+++ src/App.js
@@ -6,27 +6,49 @@ import axios from 'axios';
 
 function App() {
 
-  const [pics, setPics] = useState([]);
+  const [pics, setPics] = useState();
+  const [altText, setAltText] = useState();
+  const [info, setInfo] = useState();
 
 useEffect(() => {
   axios.get('https://api.nasa.gov/planetary/apod?api_key=0Cy7DjwTZ5Df5h9WiTizcFFsJx3B1vIHxwezah4M')
   .then(resp => {
     console.log(resp.data);
     setPics(resp.data.url);
-    
+    setAltText(resp.data.title);
+    setInfo(resp.data.explanation);
   })
   .catch(err => console.error(err))
 
-}, [])
+},)
 
   return (
     <div className="App">
-      <h1>Nasa Pic</h1>
-      <img src={pics}></img>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      
+      <div className='Header'>
+        <h1>Nasa Pic</h1>
+      </div>
+      
+      <div className='card'>
+
+      <div className='imgContainer'>
+      <img src={pics} alt={altText}/>
+      </div>
+
+      <div className='info'>
+        <h2>
+          {altText}
+        </h2>
+        <p>
+          {info}
+        </p>
+      </div>
+
+
+      </div>
+      
+      
+      
     </div>
   );
 }
